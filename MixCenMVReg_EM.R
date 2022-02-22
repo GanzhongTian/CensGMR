7ee99036816fde59471496a90c04adf222bbe40b
GanzhongTian <53979501+GanzhongTian@users.noreply.github.com>
2022-02-22 16:23:29 -0500

    Add files via upload

---
+++ MixCenMVReg_EM.R
@@ -0,0 +1,273 @@
+# source("Util_Func.R")
+
+MixCenMVReg_EM=function(Y, C, X, G=2, Max.iter=1000, pie_hat=NA, beta_hat=NA, sigma_hat=NA, diff.tol=1e-3, print=TRUE, calc_cov=TRUE){
+  
+  
+  N=dim(Y)[1]
+  P=dim(Y)[2]
+  D=dim(X)[2]
+  
+  if(is.null(colnames(Y))){
+    colnames(Y)=paste(rep('Y',P), 1:P, sep="_")
+  }
+  
+  initial=FALSE
+  
+  if(all(is.na(pie_hat)==FALSE) & all(is.na(beta_hat)==FALSE) & all(is.na(sigma_hat)==FALSE)){
+    
+    initial=TRUE
+    
+    if(length(beta_hat)==length(sigma_hat) & length(beta_hat)==length(pie_hat)){
+      G=length(beta_hat)
+    } else {
+      stop("Intial pie, beta, sigma lengths are different")
+    }
+    
+    
+    mu_hat=list()
+    for(g in 1:G){
+      mu_hat[[g]]=X%*%beta_hat[[g]]
+    }
+  }
+  
+  
+  all_obs.LogLik=c()
+  
+  # set initial iteration number:
+  iter=0
+  diff=Inf
+  # set initial conditional probabilities:
+  ind_density=matrix(NA,nrow=N,ncol=G) #individual contribution to likelihood (NxK)
+  
+  while(iter<Max.iter & diff>diff.tol){
+    
+    
+    if(iter==0 & initial==FALSE){
+      
+      ## E-step: computing the conditional posterior probabilities tau_hat
+      
+      ### First assign every one to be 1/G or a very small value
+      #             tau_hat=matrix(rep(1/G,N*G), nrow=N, ncol=G)
+      tau_hat=matrix(rep(0,N*G), nrow=N, ncol=G)
+      
+      ## Only consider uncensored case:
+#       subsample=which(apply(abs(C),1,sum)==0)
+      
+#       n=length(subsample)
+      ### Then randomly assign every one to be 1/G
+#       subsample=sample(subsample,round(n/10),replace=FALSE)
+      subsample=sample(1:N,round(N/10),replace=FALSE)
+      tau_hat[subsample,]=t(rmultinom(round(N/10), 1, rep(1,G)/G))
+        
+#       tau_hat[subsample,]=t(rmultinom(round(n/10), 1, rep(1,G)/G))
+      pie_hat=apply(tau_hat,2,mean)
+        
+      mu_hat=list()
+      for(g in 1:G){
+        mu_hat[[g]]=tau_hat[,g]*Y
+      }
+      
+      beta_hat=list()
+      for(g in 1:G){
+        beta_hat[[g]]=solve(t(X)%*%diag(tau_hat[,g])%*%X)%*%t(X)%*%diag(tau_hat[,g])%*%Y
+      }
+      
+      sigma_hat=list()
+      for(g in 1:G){
+        sigma_hat[[g]]=cov(tau_hat[,g]*Y)
+      }
+      
+      
+    }else{
+      
+      for(g in 1:G){
+        ## E-step: computing individual observation's contribution to the likelihood
+        ind_density[,g]=pie_hat[g]*mapply(eval_density, 
+                                          y=split(t(Y), rep(1:N, each = P)),
+                                          c=split(t(C), rep(1:N, each = P)), 
+                                          m=split(t(mu_hat[[g]]), rep(1:N, each = P)), 
+                                          MoreArgs=list(v=sigma_hat[[g]]))
+      }
+      
+      ## E-step: computing the conditional posterior probabilities tau_hat
+      tau_hat=ind_density/apply(ind_density,1,sum) #(NxG)
+      
+      tau_hat[is.na(tau_hat)]=1/G # adjust the nans 
+      
+      tau_hat[tau_hat< 1e-300]=1e-300 # adjust the 0    
+      
+      # Evaluate Likelihood using the ind_density for convenience
+      obs.LogLik=sum(log(apply(ind_density,1,sum)))
+      all_obs.LogLik=append(all_obs.LogLik,obs.LogLik)
+      
+      
+      
+      ## M-step: Update pie_hat
+      new_pie_hat=apply(tau_hat,2,mean)
+      diff=max(abs(pie_hat-new_pie_hat))
+      pie_hat=new_pie_hat
+      
+      ## M-step: Update beta_hat
+      Y_star=list()
+      for(g in 1:G){
+        Y_star[[g]]=t(mapply(eval_ystar, 
+                             y=split(t(Y), rep(1:N, each = P)),
+                             c=split(t(C), rep(1:N, each = P)), 
+                             m=split(t(mu_hat[[g]]), rep(1:N, each = P)), MoreArgs=list(v=sigma_hat[[g]])))
+        if(P==1){ # Fix the problem when P=1 so that mapply give a transposed result
+          Y_star[[g]]=t(Y_star[[g]])
+        }
+        colnames(Y_star[[g]])=colnames(Y)
+        
+        new_beta_hat=solve(t(X)%*%diag(tau_hat[,g])%*%X)%*%t(X)%*%diag(tau_hat[,g])%*%Y_star[[g]]   
+        diff=max(diff,max(abs(beta_hat[[g]]-new_beta_hat)))
+        beta_hat[[g]]=new_beta_hat
+        
+        mu_hat[[g]]=X%*%beta_hat[[g]]
+      } 
+      
+      ## M-step: Update sigma_hat      
+      S_star=list()
+      for(g in 1:G){
+        
+        S_star[[g]]=t(Y_star[[g]]-mu_hat[[g]])%*%(tau_hat[,g]*(Y_star[[g]]-mu_hat[[g]]))/sum(tau_hat[,g])
+        
+        r=mapply(eval_r, 
+                 y=split(t(Y), rep(1:N, each = P)),
+                 c=split(t(C), rep(1:N, each = P)), 
+                 m=split(t(mu_hat[[g]]), rep(1:N, each = P)), MoreArgs=list(v=sigma_hat[[g]]))
+        
+        R=matrix(apply(t(r)*tau_hat[,g],2,sum),nrow=P,ncol=P)/sum(tau_hat[,g])
+        
+        new_sigma_hat=S_star[[g]]+R
+        new_sigma_hat=(new_sigma_hat+t(new_sigma_hat))/2
+        
+        diff=max(diff,max(abs(sigma_hat[[g]]-new_sigma_hat)))
+        sigma_hat[[g]]=new_sigma_hat
+      }
+    }
+    
+    iter = iter + 1
+
+  } 
+    
+    
+  if(calc_cov==TRUE){
+      
+#       if(G>1){
+#         score_pie=list()
+#         for(g in 1:(G-1)){
+#           score_pie[[g]]=tau_hat[,g]/pie_hat[g]-tau_hat[,G]/pie_hat[G]
+#         }                
+#         score_pie=do.call('cbind',score_pie)
+#         colnames(score_pie)=combine(c("PIE"),1:(G-1))
+
+#       }else{
+#         score_pie=NULL
+#       }
+
+
+      score_beta=list()
+
+      for(g in 1:G){
+
+        score_beta[[g]]=t(mapply(eval_betascore, 
+                                 yc=split(t((tau_hat[,g]*(Y_star[[g]]-mu_hat[[g]])%*%solve(sigma_hat[[g]]))), rep(1:N, each = P)),
+                                 x=split(t(X), rep(1:N, each = D))))
+
+        colnames(score_beta[[g]])=combine(colnames(Y),colnames(X),c(g))
+      }
+      score_beta=do.call('cbind',score_beta)
+
+      # score_sigma=list()
+      # for(g in 1:G){
+      #     score_sigma[[g]]=tau_hat[,g]*(S_star[[g]]-(sigma_hat[g])^2)/(2*(sigma_hat[g])^4)
+      # }
+      # score_sigma=do.call('cbind',score_sigma)
+
+      # all_score=cbind(score_pie,score_beta,score_sigma)
+#       if(is.null(score_pie)){
+        all_score=cbind(score_beta)
+#       }else{
+#         all_score=cbind(score_pie,score_beta)
+#       }
+
+      obs_Info=t(all_score)%*%all_score
+
+      if(any(eigen(obs_Info)$values<=0)){
+        cov_notPD=TRUE
+        print('The Cov matrix for coefficients is not positive definite')
+
+      }else{
+        cov_notPD=FALSE
+        cov=solve(obs_Info)
+      }
+  
+  }else{
+      obs_Info=NA
+      cov_notPD=TRUE
+      print('The Info matrix for coefficients is not computed')
+      
+  }
+  
+  if(iter<Max.iter & diff<=diff.tol){
+    converge_=TRUE
+  }else{
+    converge_=FALSE
+  }
+  
+  names(pie_hat)=paste0("pie",1:G)
+  names(beta_hat)=paste0("beta",1:G)
+  names(sigma_hat)=paste0("sigma",1:G)
+  
+  if(print){
+    print(paste0("Total Iteration = ",toString(iter)))
+    print(paste0("Convergence = ",toString(converge_)))
+    print(paste0("LogLik = ",toString(round(tail(all_obs.LogLik, n=1),3))))
+    print(pie_hat)
+    print(beta_hat)
+    print(sigma_hat)
+    #         print(obs_Info)
+  }
+  
+  LogLik=tail(all_obs.LogLik, n=1)
+  nparm=length(as.vector(unlist(beta_hat)))+length(unique(as.vector(unlist(sigma_hat))))+length(pie_hat)-1
+  
+  POST_PROB=tau_hat
+  POST_PROB[POST_PROB < 1e-8] = 1e-8
+  entropy=-sum(POST_PROB*log(POST_PROB))
+  entropy.rsquare=1 - entropy/(N*log(G))    
+  
+  AIC=-2*LogLik+nparm*2
+  BIC=-2*LogLik+nparm*log(N)
+  ICL=BIC+2*entropy
+  
+  
+  
+  OUTPUT=list()
+  
+  OUTPUT[[1]]=iter
+  OUTPUT[[2]]=(iter<Max.iter)
+  OUTPUT[[3]]=LogLik
+  OUTPUT[[4]]=AIC
+  OUTPUT[[5]]=BIC
+  OUTPUT[[6]]=ICL
+  
+  OUTPUT[[7]]=pie_hat
+  OUTPUT[[8]]=beta_hat
+  OUTPUT[[9]]=sigma_hat
+  OUTPUT[[10]]=tau_hat
+  OUTPUT[[11]]=apply(tau_hat,1,eval_class)
+  if(cov_notPD){
+    OUTPUT[[12]]=obs_Info
+    names(OUTPUT)<-c("Iterations", "Converged", "LogLik","AIC", "BIC", "ICL","Pie", "Beta", "Sigma", "Posterior","Class","obs_Info")
+    
+  }else{
+    OUTPUT[[12]]=cov
+    names(OUTPUT)<-c("Iterations", "Converged", "LogLik","AIC", "BIC", "ICL","Pie", "Beta", "Sigma", "Posterior","Class","Cov")
+  }
+  
+  
+  return(OUTPUT)
+  
+}
---
+++ Util_Func.R
@@ -0,0 +1,290 @@
+eval_density=function(y,c,m,v){
+  ##############################################
+  # input:    -y    :a p-dimension vectors of continous values: observed manifest vars
+  #           -c    :a p-dimension vectors of 0,-1,1 indicating censoring directions
+  #           -m    :a p-dimension vectors of continous values: means
+  #           -v    :a pxp varcov matrix
+  #
+  # output:   a positive real value of the density
+  ##############################################
+  obslst=which(c==0);cenlst=which(c!=0)
+  #     print(length(obslst)+length(cenlst))
+  
+  if(length(obslst)>0){
+    
+    y_obs=as.vector(as.matrix(y)[obslst])
+    m_obs=as.vector(m[obslst])
+    
+    v_obs=as.matrix(v[(obslst),(obslst)])
+    
+    f_obs=dmvnorm(y_obs,m_obs,v_obs)
+  }else{
+    f_obs=1
+  }
+  
+  if(length(cenlst)>0){
+    
+    
+    lbd=rep(0,length(cenlst))
+    ubd=rep(0,length(cenlst))
+    
+    lbd[c[cenlst]==-1]=-Inf
+    lbd[c[cenlst]==1]=y[c==1]
+    
+    ubd[c[cenlst]==-1]=y[c==-1]
+    ubd[c[cenlst]==1]=Inf
+    
+    p_cen=pcmvnorm(lower=lbd, upper=ubd, mean=m, sigma=v, dep=cenlst, given=obslst, X=y[obslst])
+    
+  }else{
+    p_cen=1
+  }
+  return(unname(p_cen*f_obs))
+}
+
+# g=2
+# i=2
+# Y=Y[1:10,];C=C[1:10,];Y=Y[1:10,];M=mu_hat[[g]][1:10,];V=sigma_hat[[g]]
+# eval_density(Y[i,],C[i,],M[i,],V)
+# Y_star=mapply(eval_density, Y, C, mu_hat[[g]], sigma_hat[[g]])
+eval_ystar=function(y,c,m,v){
+  ############################################################################################
+  # input:    -y    :a p-dimension vectors of continous values: observed manifest vars
+  #           -c    :a p-dimension vectors of 0,-1,1 indicating censoring directions
+  #           -m    :a p-dimension vectors of continous values: means
+  #           -v    :a pxp varcov matrix
+  #
+  # output:   a p-dimension vector of obs_y or conditional means of cen_y
+  ############################################################################################
+  obslst=which(c==0);cenlst=which(c!=0)
+  #     print(length(obslst)+length(cenlst))
+  e_y=y
+  
+  if(length(cenlst)>0){
+    
+    lbd=rep(0,length(cenlst))
+    ubd=rep(0,length(cenlst))
+    
+    ###################lbd=rep(0,length(cenlst))
+    
+    lbd[c[cenlst]==-1]=-Inf
+    lbd[c[cenlst]==1]=y[c==1]
+    
+    ubd[c[cenlst]==-1]=y[c==-1]
+    ubd[c[cenlst]==1]=Inf
+    
+    if(length(obslst)>0){
+      
+      # lbd;ubd
+      
+      D_cen=length(cenlst)
+      
+      y_reorder=y[append(cenlst,obslst)]
+      c_reorder=c[append(cenlst,obslst)]
+      m_reorder=m[append(cenlst,obslst)]
+      
+      
+      v_reorder=v[,append(cenlst,obslst)]
+      v_reorder=as.data.frame(v_reorder[append(cenlst,obslst),])
+      # v_reorder
+      
+      v11=as.matrix(v_reorder[1:D_cen,1:D_cen])
+      v22=as.matrix(v_reorder[-(1:D_cen),-(1:D_cen)])
+      v12=as.matrix(v_reorder[1:D_cen,-(1:D_cen)])
+      
+      v_cond=v11-v12%*%solve(v22)%*%t(v12)
+      v_cond=(v_cond+t(v_cond))/2 # Force it to be symmetric
+      m_cond=as.vector(m_reorder[1:D_cen]+v12%*%solve(v22)%*%(y_reorder[-(1:D_cen)]-m_reorder[-(1:D_cen)]))
+      
+      e_y[cenlst]=as.vector(onlymeanTMD(lbd,ubd,m_cond,v_cond,dist="normal"))
+      
+      return(e_y)
+      
+    }else{
+      
+      e_y[cenlst]=as.vector(onlymeanTMD(lbd, ubd, m ,v ,dist="normal"))
+      
+      return(e_y)
+    }
+    
+  }else{ # No censoring
+    return(e_y)
+  }
+}
+
+eval_r=function(y,c,m,v){
+  ############################################################################################
+  # input:    -y    :a p-dimension vectors of continous values: observed manifest vars
+  #           -c    :a p-dimension vectors of 0,-1,1 indicating censoring directions
+  #           -m    :a p-dimension vectors of continous values: means
+  #           -v    :a pxp varcov matrix
+  #
+  # output:   a pxp cond_varcov matrix subjected to 0s for the obs features
+  ############################################################################################
+  obslst=which(c==0);cenlst=which(c!=0)
+  #     print(length(obslst)+length(cenlst))
+  D_tot=length(y)
+  #     print("D_tot")
+  #     print(D_tot)
+  
+  S_star_reorder=matrix(rep(0,D_tot^2),nrow=D_tot,ncol=D_tot)
+  #     print(S_star_reorder)
+  if(length(cenlst)>0){
+    
+    lbd=rep(0,length(cenlst))
+    ubd=rep(0,length(cenlst))
+    
+    lbd[c[cenlst]==-1]=-Inf
+    lbd[c[cenlst]==1]=y[c==1]
+    
+    ubd[c[cenlst]==-1]=y[c==-1]
+    ubd[c[cenlst]==1]=Inf
+    
+    if(length(obslst)>0){
+      
+      # lbd;ubd
+      
+      D_cen=length(cenlst)
+      
+      y_reorder=y[append(cenlst,obslst)]
+      c_reorder=c[append(cenlst,obslst)]
+      m_reorder=m[append(cenlst,obslst)]
+      
+      
+      v_reorder=v[,append(cenlst,obslst)]
+      v_reorder=as.data.frame(v_reorder[append(cenlst,obslst),])
+      
+      v11=as.matrix(v_reorder[1:D_cen,1:D_cen])
+      v22=as.matrix(v_reorder[-(1:D_cen),-(1:D_cen)])
+      v12=as.matrix(v_reorder[1:D_cen,-(1:D_cen)])
+      
+      v_cond=unname(v11-v12%*%solve(v22)%*%t(v12))
+      v_cond=(v_cond+t(v_cond))/2
+      m_cond=as.vector(m_reorder[1:D_cen]+v12%*%solve(v22)%*%(y_reorder[-(1:D_cen)]-m_reorder[-(1:D_cen)]))
+      
+      subS_star_reorder=meanvarTMD(lbd,ubd,m_cond,v_cond,dist="normal")$varcov
+      #             subS_star_reorder=MCmeanvarTMD(lbd,ubd,m_cond,v_cond,dist="normal")$EYY
+      
+      S_star_reorder=matrix(rep(0,D_tot^2),nrow=D_tot,ncol=D_tot)
+      S_star_reorder[1:D_cen,1:D_cen]=subS_star_reorder
+      
+      # # colnames(S_star_reorder)=append(cenlst,obslst)
+      # # rownames(S_star_reorder)=append(cenlst,obslst)
+      # # S_star_reorder
+      
+      S_star=S_star_reorder[,match(1:D_tot,append(cenlst,obslst))]
+      S_star=S_star[match(1:D_tot,append(cenlst,obslst)),]
+      #              print(S_star)
+      return(S_star)
+      
+    }else{
+      
+      S_star=meanvarTMD(lower=lbd,upper=ubd,mu=m,Sigma=v,dist="normal")$varcov
+      #             S_star=MCmeanvarTMD(lower=lbd,upper=ubd,mu=m,Sigma=v,dist="normal")$EYY
+      
+      return(S_star)
+    }
+    
+  }else{ # No censoring
+    S_star=S_star_reorder
+    return(S_star)
+  }
+}
+
+eval_betascore=function(yc,x){
+  ############################################################################################
+  # input:    -yc      :a 1xp response vector of continous values: centered y_star
+  #           -x       :a 1xd design matrix of covariates (if intercepts are considered, should have a col of 1s) #
+  # output:   a vectorized dxp matrix as the individual score of beta taking the same dimension as beta.
+  ############################################################################################
+  return(as.vector(x%*%t(yc)))
+}
+
+combine=function(..., prefix='', sep='_') {
+  combine.inner=function(lx, ...) {
+    if (length(c(...)) > 0) {
+      sapply(sapply(lx, function(x) paste(x, combine.inner(...), sep=sep)), c)
+    } else {
+      lx
+    }
+  }
+  paste(prefix, combine.inner(...), sep='')
+}
+
+eval_obslogLik=function(y,c,x,pie,beta,sigma){
+  ############################################################################################
+  # input:    -y      :a nxp response matrix of continous values: subject to censoring.
+  #           -c      :a nxp censorship matrix of 0,-1,1 indicating censoring directions.
+  #           -x      :a nxd design matrix of covariates (if intercepts are considered, should have a col of 1s) #
+  #           -pie    :a length G vector of mixing proportions which add up to 1.
+  #           -beta   :a length G list of pxd coeficients matrices (loading matrices).
+  #           -sigma  :a length G list of pxp varcov matrices.
+  #
+  # output:   a scalar of observed incomplete logLikelihood.
+  ############################################################################################
+  
+  #determine the sample size and dimension
+  if(dim(y)[1]==dim(c)[1] & dim(c)[1]==dim(x)[1]){
+    N=dim(y)[1]
+  } else {
+    stop("sample sizes in y, c, x are different")
+  }
+  
+  if(dim(y)[2]==dim(c)[2]){ #maybe consider sigma also?????
+    P=dim(y)[2]
+  } else {
+    stop("feature dimensions in y, c are different")
+  }
+  
+  
+  if(length(beta)==length(sigma) & length(beta)==length(pie)){
+    G=length(beta)
+  } else {
+    stop("pie, beta, sigma lengths are different")
+  }
+  
+  
+  lik=rep(0, N)
+  for(g in 1:G){
+    lik=lik+pie[g]*(dnorm(df$Y,as.matrix(df[c('X0','X1')])%*%beta[[g]],sigma[[g]])*(1-df$left_cenored)+
+                      pnorm(df$Y,as.matrix(df[c('X0','X1')])%*%beta[[g]],sigma[[g]])*df$left_cenored)
+  }
+  LogLik=sum(log(lik))
+}
+                    
+eval_subdensity=function(Y,C,X,beta_hat,sigma_hat){
+    
+    N=dim(Y)[1]
+    P=dim(Y)[2]
+    D=dim(X)[2]
+    
+    if(length(beta_hat)==length(sigma_hat)){
+      G=length(beta_hat)
+    } else {
+      stop("Intial beta, sigma lengths are different")
+    }
+    
+    
+    mu_hat=list()
+    
+    for(g in 1:G){
+      mu_hat[[g]]=X%*%beta_hat[[g]]
+    }
+    
+    ind_density=matrix(NA,nrow=N,ncol=G) #individual contribution to likelihood (NxK)
+    for(g in 1:G){
+        ## E-step: computing individual observation's contribution to the likelihood
+        ind_density[,g]=mapply(eval_density, 
+                                          y=split(t(Y), rep(1:N, each = P)),
+                                          c=split(t(C), rep(1:N, each = P)), 
+                                          m=split(t(mu_hat[[g]]), rep(1:N, each = P)), 
+                                          MoreArgs=list(v=sigma_hat[[g]]))
+    }
+    return(ind_density)
+}
+                    
+                    
+                    
+eval_class=function(p){
+  return(which(p==max(p)))
+}
